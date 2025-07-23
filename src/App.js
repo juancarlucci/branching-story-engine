dbbe6dffd941f319d368870c9f12d13249bda3d9
juancarlucci <juancarloscollins@gmail.com>
2025-07-22 21:43:48 -0700

    chore(init): scaffold Create React App and clean up default boilerplate

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import DataTable from "./components/DataTable-07-pagination";
+
+import "./App.css";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <main className="app-container">
+      <h1>RenderFlow</h1>
+      <DataTable />
+    </main>
+  );
+}
