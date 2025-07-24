c43a759b9ac0adbae698f841e47b09f36bba99f5
juancarlucci <juancarloscollins@gmail.com>
2025-07-24 13:34:50 -0700

    feat(HomePage) creates component

---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import HeroBanner from "../components/HeroBanner";
+
+export default function HomePage() {
+  return (
+    <>
+        {/* hero, carousel, tags, list */}
+        <HeroBanner />
+    </>
+  );
+}
