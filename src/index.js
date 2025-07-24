6bfdb30df3fe86e6fb00b0152c7ee31729bf51f9
juancarlucci <juancarloscollins@gmail.com>
2025-07-24 13:30:18 -0700

    feat(routing) adds react-router-dom and BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Layout from "./pages/Layout";
+import HomePage from "./pages/HomePage";
+import BookDetail from "./components/BookDetail";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+  <Route path="/" element={<Layout><HomePage /></Layout>} />
+  <Route path="/products/:id" element={<Layout><BookDetail /></Layout>} />
+</Routes>
+  </BrowserRouter>
 );
 
 
