d2d8ffe6a9efbed2c33ad926967ca533cc3d0856
juancarlucci <juancarloscollins@gmail.com>
2025-07-24 13:28:30 -0700

    feat(NavBar.js): create fixed top navigation bar with logo and links

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import "./NavBar.css"; 
+
+export default function NavBar() {
+  return (
+    <header className="navbar-header">
+      <Link to="/" className="navbar-logo">
+        GalateaClone
+      </Link>
+      <nav className="navbar-links">
+            <Link to="/categories" style={{ color: "#ccc", whiteSpace: "nowrap" }}>Categories</Link>
+            <Link to="/login" style={{ color: "#ccc", whiteSpace: "nowrap" }}>Log In</Link>
+            </nav>
+    </header>
+  );
+}
