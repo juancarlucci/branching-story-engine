3784f000b7995551eb816c90106c2efa43e895b8
juancarlucci <juancarloscollins@gmail.com>
2025-08-05 16:26:52 -0700

    feat(index.js): add AllCategorisPage to router

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import HomePage from "./pages/HomePage";
 import BookDetail from "./components/BookDetail";
+import AllCategoriesPage from "./pages/AllCategories";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -13,6 +14,8 @@ root.render(
     <Routes>
   <Route path="/" element={<Layout><HomePage /></Layout>} />
   <Route path="/products/:id" element={<Layout><BookDetail /></Layout>} />
+  <Route path="/categories" element={<AllCategoriesPage />} /> 
+  <Route path="/books/:id" element={<BookDetail />} />
 </Routes>
   </BrowserRouter>
 );
