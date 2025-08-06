8232ae663e151f343d7ca93bd9b32a15019a4cf5
juancarlucci <juancarloscollins@gmail.com>
2025-08-05 18:50:45 -0700

    feat(NavBar): add responsive Galatea and Inkitt logo using local webp assets

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,9 +6,33 @@ export default function NavBar() {
   return (
     <header className="navbar-header">
       <div className="navbar-inner">
-        <Link to="/" className="navbar-logo">
-          GalateaClone
+        <Link to="/" className="navbar-logo-group" aria-label="Go to homepage">
+          {/* Mobile-only vertical logo */}
+          <img
+            className="logo-vertical"
+            src="/galatea-logo-solo.webp"
+            alt="Galatea logo"
+            width="110"
+            height="32"
+          />
+          {/* Desktop logo row */}
+          <div className="logo-horizontal">
+            <img
+              src="/galatea-logo-solo.webp"
+              alt="Galatea logo"
+              width="123"
+              height="32"
+            />
+            <span className="logo-by">by</span>
+            <img
+              src="/inkitt-logo.webp"
+              alt="Inkitt logo"
+              width="44"
+              height="14"
+            />
+          </div>
         </Link>
+
         <nav className="navbar-links">
           <Link to="/categories">Categories</Link>
           <Link to="/login">Log In</Link>
@@ -18,3 +42,4 @@ export default function NavBar() {
     </header>
   );
 }
+
