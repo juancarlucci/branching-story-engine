5134950ace057e0f92bf99bfab4dced6fc75e4cb
juancarlucci <juancarloscollins@gmail.com>
2025-10-15 09:15:00 -0700

    fix(NavBar): apply scoped styles correctly

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,20 @@
 // @ts-nocheck
 import { Link } from "react-router-dom";
-import "./NavBar.css"; 
+import "./NavBar.css";
 
 export default function NavBar() {
   return (
     <header className="navbar-header">
-      <Link to="/" className="navbar-logo">
-        GalateaClone
-      </Link>
-      <nav className="navbar-links">
-            <Link to="/categories" style={{ color: "#ccc", whiteSpace: "nowrap" }}>Categories</Link>
-            <Link to="/login" style={{ color: "#ccc", whiteSpace: "nowrap" }}>Log In</Link>
-            </nav>
+      <div className="navbar-inner">
+        <Link to="/" className="navbar-logo">
+          GalateaClone
+        </Link>
+        <nav className="navbar-links">
+          <Link to="/categories">Categories</Link>
+          <Link to="/login">Log In</Link>
+          <Link to="/story">Story Engine</Link>
+        </nav>
+      </div>
     </header>
   );
 }
