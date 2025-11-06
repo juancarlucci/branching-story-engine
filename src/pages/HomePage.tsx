d244d261fa322ac218defaee6d3a60ca50d2b727
juancarlucci <juancarloscollins@gmail.com>
2025-11-06 09:10:00 -0800

    fix(Home page) remove rdundant dependency

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -16,7 +16,7 @@ export default function HomePage() {
   useEffect(() => {
     setBooks(mockBooks);
     setLoading(false);
-  }, [books]);
+  }, []);
 
   return (
     <div className="homepage-wrapper">
