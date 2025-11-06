54a987202dd872f140191a7387ffe807d9ba96ef
juancarlucci <juancarloscollins@gmail.com>
2025-11-06 09:00:00 -0800

    chore(index): wrap app in QueryClientProvider for React Query

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 
 import Layout from "./pages/Layout";
 import HomePage from "./pages/HomePage";
@@ -11,17 +13,20 @@ import StoryEngine from "./pages/StoryEngine";
 
 import "./index.css";
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <Routes>
-  <Route path="/" element={<Layout><HomePage /></Layout>} />
-  <Route path="/products/:id" element={<Layout><BookDetail /></Layout>} />
-  <Route path="/categories" element={<AllCategoriesPage />} /> 
-  <Route path="/books/:id" element={<BookDetail />} />
-  <Route path="/story" element={<Layout><StoryEngine /></Layout>} />
-</Routes>
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout><HomePage /></Layout>} />
+        <Route path="/products/:id" element={<Layout><BookDetail /></Layout>} />
+        <Route path="/categories" element={<AllCategoriesPage />} /> 
+        <Route path="/books/:id" element={<BookDetail />} />
+        <Route path="/story" element={<Layout><StoryEngine /></Layout>} />
+      </Routes>
+    </BrowserRouter>
+  </QueryClientProvider>
 );
 
 
